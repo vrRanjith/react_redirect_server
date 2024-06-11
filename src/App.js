bf945899d180d87c5a5339875154fd6ff3afa08c
ranjith <vranju2001@gmail.com>
2024-06-11 16:20:27 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React, { useEffect } from 'react';
+
+function RedirectComponent() {
+  useEffect(() => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const code = urlParams.get('code');
+    if (code) {
+      window.location.href = `myapp://callback?code=${code}`;
+    }
+  }, []);
+
+  return <p>Redirecting...</p>;
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <RedirectComponent />
     </div>
   );
 }
